d7d64adbf007a418ded5245629bd43ad076bbf95
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-09-09 15:35:21 +0530

    comp(Modal): implement apps
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -10,13 +10,35 @@ class Modal extends React.Component {
 
         this.state = {
             workflow: {},
+            selectedApp: "",
             selectedNode: "",
             inputData: {},
+            connectWindow: null,
         };
     }
 
+    connectApp = () => {
+        cobalt.getAppAuthStatus(this.state.selectedApp)
+        .then(connected => {
+            if (connected === true) {
+                // TODO: set connected status
+                console.log(this.state.selectedApp, "ALREADY CONNECTED", connected);
+                this.state.connectWindow?.close();
+                this.setState({ connectWindow: null });
+            } else {
+                cobalt.getAppAuthUrl(this.state.selectedApp)
+                .then(authUrl => {
+                    const connectWindow = window.open(authUrl);
+                    this.setState({ connectWindow });
+                })
+                .catch(console.error);
+            }
+        })
+        .catch(console.error);
+    };
+
     saveNode = () => {
-        cobalt.saveNode(state.workflow?.workflow_id, this.state.selectedNode, this.state.inputData)
+        cobalt.saveNode(this.state.workflow?.workflow_id, this.state.selectedNode, this.state.inputData)
         .then(console.log)
         .catch(console.error);
     };
@@ -32,6 +54,7 @@ class Modal extends React.Component {
         cobalt.token = "";
 
         this.setState({
+            selectedApp: "",
             selectedNode: "",
         });
     }
@@ -51,6 +74,26 @@ class Modal extends React.Component {
                         </div>
                         <div style={ styles.sidebarContent }>
                             {
+                                this.state.workflow?.applications?.map(app =>
+                                    <div
+                                        key={ app }
+                                        style={{
+                                            ...styles.sidebarItem,
+                                            ...(this.state.selectedApp === app ? styles.sidebarItemSelected : {}),
+                                        }}
+                                        onClick={ () => this.setState({ selectedApp: app, selectedNode: "" }) }
+                                    >
+                                        <img
+                                            src={ "https://img.icons8.com/color/48/000000/checked--v1.png" }
+                                            width={ 20 }
+                                            height={ 20 }
+                                            style={ styles.sidebarItemIcon }
+                                        />
+                                        <div style={{ textTransform: "capitalize" }}>Connect { app.replaceAll("_", " ") }</div>
+                                    </div>
+                                )
+                            }
+                            {
                                 this.state.workflow?.configure?.map(node =>
                                     <div
                                         key={ node.node_id }
@@ -58,7 +101,7 @@ class Modal extends React.Component {
                                             ...styles.sidebarItem,
                                             ...(this.state.selectedNode === node.node_id ? styles.sidebarItemSelected : {}),
                                         }}
-                                        onClick={ () => this.setState({ selectedNode: node.node_id }) }
+                                        onClick={ () => this.setState({ selectedApp: "", selectedNode: node.node_id }) }
                                     >
                                         <img
                                             src={ node.node_icon }
@@ -74,42 +117,67 @@ class Modal extends React.Component {
                     </div>
                     <div style={ styles.section }>
                         {
-                            this.state.selectedNode && (
-                                <div style={ styles.contentHeader }>
-                                    <img
-                                        src={ this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.node_icon }
-                                        width={ 32 }
-                                        height={ 32 }
-                                        style={ styles.sidebarItemIcon }
-                                    />
-                                    <h3>{ this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.node_name }</h3>
-                                </div>
+                            this.state.selectedApp && (
+                                <React.Fragment>
+                                    <div style={ styles.contentHeader }>
+                                        <img
+                                            src={ "https://img.icons8.com/color/48/000000/checked--v1.png" }
+                                            width={ 32 }
+                                            height={ 32 }
+                                            style={ styles.sidebarItemIcon }
+                                        />
+                                        <h3 style={{ textTransform: "capitalize" }}>{ this.state.selectedApp.replaceAll("_", " ") }</h3>
+                                    </div>
+                                    <div key={ this.state.selectedApp } style={ styles.contentBody }>
+
+                                    </div>
+                                    <div style={ styles.contentFooter }>
+                                        <button style={ styles.button } onClick={ this.connectApp }>
+                                            Connect
+                                        </button>
+                                    </div>
+                                </React.Fragment>
                             )
                         }
-                        <div key={ this.state.selectedNode } style={ styles.contentBody }>
-                            {
-                                this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.fields?.map(field =>
-                                    <div key={ field.name }>
-                                        <div style={ styles.inputLabel }>{ field.name }</div>
-                                        <input
-                                            type={ field.type }
-                                            placeholder={ field.placeholder }
-                                            required={ field.required }
-                                            style={ styles.inputText }
-                                            value={ this.state.inputData[field.name] }
-                                            onChange={ e => this.setState({ inputData: { ...this.state.inputData, [field.name]: e.target.value }}) }
-                                        />
-                                    </div>
-                                )
-                            }
-                        </div>
                         {
                             this.state.selectedNode && (
-                                <div style={ styles.contentFooter }>
-                                    <button style={ styles.button } onClick={ this.saveNode }>
-                                        Save
-                                    </button>
-                                </div>
+                                <React.Fragment>
+                                    <div style={ styles.contentHeader }>
+                                        <img
+                                            src={ this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.node_icon }
+                                            width={ 32 }
+                                            height={ 32 }
+                                            style={ styles.sidebarItemIcon }
+                                        />
+                                        <h3>{ this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.node_name }</h3>
+                                    </div>
+                                    <div key={ this.state.selectedNode } style={ styles.contentBody }>
+                                        {
+                                            this.state.workflow?.configure?.find(n => n.node_id === this.state.selectedNode)?.fields?.map(field =>
+                                                <div key={ field.name }>
+                                                    <div style={ styles.inputLabel }>{ field.name }</div>
+                                                    <input
+                                                        type={ field.type }
+                                                        placeholder={ field.placeholder }
+                                                        required={ field.required }
+                                                        style={{
+                                                            ...styles.inputText,
+                                                            marginBottom: field.multiple ? 2 : 15,
+                                                        }}
+                                                        value={ this.state.inputData[field.name]?.value }
+                                                        onChange={ e => this.setState({ inputData: { ...this.state.inputData, [field.name]: { value: field.multiple ? e.target.value?.split(",") : e.target.value }}}) }
+                                                    />
+                                                    { field.multiple && <div style={ styles.inputHelp }>Accepts comma separated values.</div> }
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                    <div style={ styles.contentFooter }>
+                                        <button style={ styles.button } onClick={ this.saveNode }>
+                                            Save
+                                        </button>
+                                    </div>
+                                </React.Fragment>
                             )
                         }
                     </div>
@@ -212,6 +280,11 @@ const styles = {
         padding: "10px",
         border: "1px solid #dfe3e8",
         borderRadius: 8,
+    },
+    inputHelp: {
+        marginBottom: 15,
+        fontSize: 12,
+        opacity: .4,
     },
     button: {
         padding: "10px 20px",
