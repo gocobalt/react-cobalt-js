b1506348235b1755d4c692da50fb9f6535cd4bf8
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-09-12 16:06:42 +0530

    feat: include cobalt sdk instance in context
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Provider/index.jsx
+++ src/components/Provider/index.jsx
@@ -1,12 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
+import Cobalt from "@cobaltio/cobalt-js";
+
+const cobalt = new Cobalt({ baseUrl: "https://embedapi.gocobalt.io" });
 
 export const Context = createContext();
 
 export const Provider = ({ children, sessionToken }) => {
-    const [ token, ] = useState(sessionToken || "");
-
     return (
-        <Context.Provider value={[ token ]}>
+        <Context.Provider value={{
+            cobalt,
+            sessionToken,
+        }}>
             { children }
         </Context.Provider>
     );
