5ce3ec79ddd4b6ebe143bb573c30a2293c07fad3
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-09-13 16:08:35 +0530

    comp(Footer): update style
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Installer/Footer/index.jsx
+++ src/components/Installer/Footer/index.jsx
@@ -58,7 +58,7 @@ const Footer = ({ disabled }) => {
             >
                 { selectedItem ? workflow?.configure?.some(n => n.node_id === selectedItem) ? "Save" : "Authorize" : "Proceed" }
             </button>
-            <div style={{ marginTop: 10, textAlign: "center", fontSize: 12, opacity: .5 }}>Powered by <a href="https://gocobalt.io" target="_blank">Cobalt</a></div>
+            <div style={{ marginTop: 5, textAlign: "center", fontSize: 12, opacity: .5 }}>Powered by <a href="https://gocobalt.io" target="_blank">Cobalt</a></div>
         </div>
     )
 };
