59131fdf217b0e13fc20308059f6f2002ce949d4
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-10-18 16:59:56 +0530

    feat: add support for custom base api
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Provider/index.jsx
+++ src/components/Provider/index.jsx
@@ -1,11 +1,11 @@
 import React, { createContext } from "react";
 import Cobalt from "@cobaltio/cobalt-js";
 
-const cobalt = new Cobalt({ baseUrl: "https://embedapi.gocobalt.io" });
-
 export const Context = createContext();
 
-export const Provider = ({ children, sessionToken, theme }) => {
+export const Provider = ({ children, baseApi, sessionToken, theme }) => {
+    const cobalt = new Cobalt({ baseUrl: baseApi || "https://api.gocobalt.io" });
+
     return (
         <Context.Provider value={{
             cobalt,
