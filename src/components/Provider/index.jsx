25fa65b51fec6c0e0d28af2fdd27865544317878
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-01-20 14:01:59 +0530

    comp(Provider): include MUI CssVarsProvider
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Provider/index.jsx
+++ src/components/Provider/index.jsx
@@ -1,5 +1,6 @@
 import React, { createContext } from "react";
 import Cobalt from "@cobaltio/cobalt-js";
+import { CssVarsProvider } from "@mui/joy";
 
 export const Context = createContext();
 
@@ -7,12 +8,14 @@ export const Provider = ({ children, baseApi, sessionToken, theme }) => {
     const cobalt = new Cobalt({ baseUrl: baseApi || "https://api.gocobalt.io" });
 
     return (
-        <Context.Provider value={{
-            cobalt,
-            sessionToken,
-            theme: theme === "dark" ? "dark" : "light",
-        }}>
-            { children }
-        </Context.Provider>
+        <CssVarsProvider>
+            <Context.Provider value={{
+                cobalt,
+                sessionToken,
+                theme: theme === "dark" ? "dark" : "light",
+            }}>
+                { children }
+            </Context.Provider>
+        </CssVarsProvider>
     );
 };
