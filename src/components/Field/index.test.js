64887390b00eafad8c71bc37b3803667968f7578
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-05-19 14:59:07 +0530

    tests: add Field comp test
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

---
+++ src/components/Field/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Field from ".";
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Field />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
