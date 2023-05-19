bbf815a539f4ed9df766e37865c57b1417425cfe
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-05-19 14:47:49 +0530

    cobalt: export Field component
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Config from "./components/Config";
+import Field from "./components/Field";
 import { Context, Provider } from "./components/Provider";
 
-export { Context, Config, Provider };
+export { Context, Config, Field, Provider };
