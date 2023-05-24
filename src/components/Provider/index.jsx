7bca6641e2fae2fee83383077dc94054e7ef0962
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-05-24 17:22:18 +0530

    comp(Provider): add theme overrides
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Provider/index.jsx
+++ src/components/Provider/index.jsx
@@ -1,6 +1,6 @@
 import React, { createContext } from "react";
 import Cobalt from "@cobaltio/cobalt-js";
-import { CssVarsProvider } from "@mui/joy";
+import { CssVarsProvider, extendTheme } from "@mui/joy";
 
 /**
  * @typedef Context
@@ -19,17 +19,75 @@ export const Context = createContext();
  * @param {import("react").ReactNode} children
  * @param {String} [props.baseApi] Base URL for Cobalt
  * @param {String} props.sessionToken Session Token
- * @param {"dark"|"light"|"system"} props.theme
  */
-export const Provider = ({ children, baseApi, sessionToken, theme }) => {
+export const Provider = ({ children, baseApi, sessionToken }) => {
     const cobalt = new Cobalt({ baseUrl: baseApi || "https://api.gocobalt.io" });
+    const customTheme = extendTheme({
+        components: {
+            JoyInput: {
+                defaultProps: {
+                    size: "lg",
+                },
+                styleOverrides: {
+                    root: {
+                        borderColor: "rgba(145, 158, 171, 0.24)",
+                    },
+                },
+            },
+            JoySelect: {
+                defaultProps: {
+                    size: "lg",
+                },
+                styleOverrides: {
+                    listbox: {
+                        boxShadow: `0 0 2px 0 rgba(145, 158, 171, 0.24), -20px 20px 40px -4px rgba(145, 158, 171, 0.24)`,
+                        borderColor: "rgba(145, 158, 171, 0.24)",
+                    },
+                    root: {
+                        borderColor: "rgba(145, 158, 171, 0.24)",
+                    },
+                },
+            },
+            JoyTextarea: {
+                defaultProps: {
+                    size: "lg",
+                },
+                styleOverrides: {
+                    root: {
+                        borderColor: "rgba(145, 158, 171, 0.24)",
+                    },
+                },
+            },
+            JoySheet: {
+                styleOverrides: {
+                    root: {
+                        borderColor: "rgba(145, 158, 171, 0.12)"
+                    },
+                },
+            },
+            JoyTab: {
+                styleOverrides: {
+                    root: {
+                        fontWeight: 600,
+                    },
+                },
+            },
+            JoyDivider: {
+                styleOverrides: {
+                    root: {
+                        backgroundColor: "rgba(145, 158, 171, 0.24)"
+                    },
+                },
+            },
+        },
+    });
 
     return (
-        <CssVarsProvider defaultColorScheme={ theme === "dark" ? "dark" : "light" }>
+        <CssVarsProvider defaultColorScheme="light" theme={ customTheme }>
             <Context.Provider value={{
                 cobalt,
                 sessionToken,
-                theme: theme === "dark" ? "dark" : "light",
+                theme: "light",
             }}>
                 { children }
             </Context.Provider>
