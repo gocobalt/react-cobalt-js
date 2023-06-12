596bf59212f356bd671065b249a816c29a2c9894
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-06-12 14:57:15 +0530

    example: update example for v1
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- example/src/App.js
+++ example/src/App.js
@@ -1,106 +1,62 @@
-import React, { useEffect, useState } from "react";
-// import the Cobalt Provider & Installer components
-import { Provider as CobaltProvider, Installer as CobaltInstaller } from "@cobaltio/react-cobalt-js";
+import { useEffect, useState } from "react";
+// import the Cobalt Provider & Config components
+import {
+    Provider as CobaltProvider,
+    Config as CobaltConfig,
+} from "@cobaltio/react-cobalt-js";
 
 const App = () => {
-    const [ cobaltToken, setCobaltToken ] = useState(null);
-    const [ templates, setTemplates ] = useState([]);
-    const [ workflows, setWorkflows ] = useState([]);
-    const [ selectedTemplate, setSelectedTemplate ] = useState(null);
-    const [ selectedWorkflow, setSelectedWorkflow ] = useState(null);
+    const [ sessionToken, setSessionToken ] = useState(null);
+    const [ apps, setApps ] = useState([]);
+    const [ selectedApp, setSelectedApp ] = useState(null);
 
-    // generate cobalt session token using cobalt backend sdk
-    const generateCobaltToken = () => {
-        fetch("/api/token", {
-            method: "POST",
-            body: JSON.stringify({
-                user: localStorage.getItem("LINKED_ACCOUNT_ID"),
-            }),
-        })
-        .then(res => res.json())
-        .then(data => setCobaltToken(data.token))
+    // get Cobalt session token from Cobalt backend SDK
+    const fetchSessionToken = () => {
+        fetch("/api/token")
+        .then(data => setSessionToken(data.token))
         .catch(console.error);
     };
 
     useEffect(() => {
-        // generate cobalt token and store it
-        generateCobaltToken();
-
-        // get all workflow templates from Cobalt using the backend SDK
-        fetch("/api/templates")
-        .then(res => res.json())
-        .then(setTemplates)
-        .catch(console.error);
-
-        // get all installed workflows from Cobalt using the backend SDK
-        fetch("/api/workflows")
-        .then(res => res.json())
-        .then(setWorkflows)
-        .catch(console.error);
+        // get Cobalt session token and store it
+        fetchSessionToken();
     }, []);
 
     return (
-        // pass the generated cobalt token to the Cobalt Provider and
-        // wrap the Cobalt Provider component around the Cobalt Installer component
-        <CobaltProvider sessionToken={ cobaltToken }>
+        // pass the cobalt session token to the Cobalt `Provider` and
+        // wrap the Cobalt `Provider` component around the Cobalt `Config` component
+        <CobaltProvider sessionToken={ sessionToken }>
             <div className="Page">
-                <div className="Header">Templates</div>
+                <div className="Header">Token: <code>{ sessionToken }</code></div>
+                <div className="Header">Applications</div>
                 <div className="Templates">
-                    {
-                        // render the workflow templates you get from cobalt backend sdk
-                        templates.map(t =>
-                            <div className="">
-                                <div>{ t.name }</div>
-                                <div>{ t.description }</div>
-                                <button onClick={ () => {
-                                    setSelectedTemplate(t._id);
-                                    setSelectedWorkflow(null);
-                                }}>
-                                    Install
-                                </button>
-                            </div>
-                        )
-                    }
-                </div>
-
-                <div className="Header">Workflows</div>
-                <div className="Workflows">
-                    {
-                        // render the installed workflows you get from cobalt backend sdk
-                        workflows.map(wf =>
-                            <div className="">
-                                <div>{ wf.name }</div>
-                                <div>{ wf.description }</div>
-                                <button onClick={ () => {
-                                    setSelectedTemplate(null);
-                                    setSelectedWorkflow(wf._id);
-                                }}>
-                                    Edit
-                                </button>
-                            </div>
-                        )
-                    }
+                    {/* you fetch all the enabled apps from Cobalt backend SDK and render them dynamically. */}
+                    <div>
+                        <div>Slack</div>
+                        <button onClick={ () => setSelectedApp("slug") }>
+                            Configure
+                        </button>
+                    </div>
+                    <div>
+                        <div>Twilio</div>
+                        <button onClick={ () => setSelectedApp("twilio") }>
+                            Configure
+                        </button>
+                    </div>
                 </div>
 
                 { /*
-                   * render the workflow template installer in a modal, inline or however you want
-                   * by passing the workflow template id to the installer
+                   * render the `Config` component in a modal, inline or however you want
+                   * by passing the app's `slug` (or `type`, if it's a native app) to the component.
                    */ }
-                <dialog open={ !!selectedTemplate }>
-                    <CobaltInstaller
-                        templateId={ selectedTemplate }
-                        onInstall={ () => setSelectedTemplate(null) }
-                    />
-                </dialog>
-
-                { /*
-                   * render the workflow editor in a modal, inline or however you want
-                   * by passing the workflow id to the component
-                   */ }
-                <dialog open={ !!selectedWorkflow }>
-                    <CobaltInstaller
-                        workflowId={ selectedWorkflow }
-                        onClose={ () => setSelectedTemplate(null) }
+                <dialog open={ !!selectedApp }>
+                    <CobaltConfig
+                        id="CONFIG_ID" // Optional.
+                        slug={ selectedApp }
+                        style={{
+                            borderRadius: 8,
+                            maxWidth: 450,
+                        }}
                     />
                 </dialog>
             </div>
