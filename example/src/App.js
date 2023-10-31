5953589d2a49afa67ba589b27a3baccef7028849
TRACTION <19631364+iamtraction@users.noreply.github.com>
2023-10-31 12:22:32 +0530

    version: 1.0.0-beta.13
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "@cobaltio/react-cobalt-js";
 
 const App = () => {
-    const [ sessionToken, setSessionToken ] = useState(null);
+    const [ sessionToken, setSessionToken ] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiI2M2IzY2E1NzczYTIxMjQzMTZkMDRiNTgiLCJsaW5rZWRfYWNjb3VudF9pZCI6InRlc3QiLCJlbnZpcm9ubWVudCI6InRlc3QiLCJpYXQiOjE2OTY1OTMyOTMsImV4cCI6MTY5Njg1MjQ5M30.kQ0uU23xr_yqY_KwAt7Dd0gsVWn8cLJOvm3ZLjvgFOs");
     const [ selectedApp, setSelectedApp ] = useState(null);
 
     // get Cobalt session token from Cobalt backend SDK
@@ -18,13 +18,13 @@ const App = () => {
 
     useEffect(() => {
         // get Cobalt session token and store it
-        fetchSessionToken();
+        // fetchSessionToken();
     }, []);
 
     return (
         // pass the cobalt session token to the Cobalt `Provider` and
         // wrap the Cobalt `Provider` component around the Cobalt `Config` component
-        <CobaltProvider sessionToken={ sessionToken }>
+        <CobaltProvider baseApi="https://embedapi.gocobalt.io" sessionToken={ sessionToken }>
             <div className="Page">
                 <div className="Header">Token: <code>{ sessionToken }</code></div>
                 <div className="Header">Applications</div>
