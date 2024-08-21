9ffa37da7d7f905c27cccc604fc08820f67770bf
TRACTION <19631364+iamtraction@users.noreply.github.com>
2024-08-21 13:15:01 +0530

    comp(DataSlot): implement new UI components
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/Field/DataSlot.jsx
+++ src/components/Field/DataSlot.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Input, Option, Select, Stack, Textarea, Typography } from "@mui/joy";
 
-const handleSelectValue = (value, isMultiple) => {
-    if (isMultiple) {
-        if (value instanceof Array) return value;
-        return [];
-    }
-    if (typeof value !== "undefined" && typeof value !== "object") return value;
-    return "";
-};
+import Input from "../ui/Input";
+import Select from "../ui/Select";
+import SelectItem from "../ui/SelectItem";
+import Textarea from "../ui/Textarea";
+import Fieldset from "../ui/Fieldset";
+import Combobox from "../ui/Combobox";
 
 const DataSlot = ({
     type,
@@ -21,53 +18,42 @@ const DataSlot = ({
 }) => {
     if (type === "map") {
         return (
-            <Stack spacing={ 1 }>
-                {
-                    labels?.map(label =>
-                        <Stack key={ label.value } direction="row" alignItems="center" spacing={ 1 }>
-                            <Typography flex={ 1 } sx={{ wordBreak: "break-word", whiteSpace: "pre-wrap" }}>{ label.name }</Typography>
-                            <Select
-                                name={ label.value }
-                                placeholder="Select"
-                                multiple={ label.multiple }
-                                value={ handleSelectValue(value?.[label.value], label.multiple) }
-                                onChange={ (_, v) => onChange({ ...value, [label.value]: v }) }
-                                sx={{ flex: 1 }}
-                            >
-                                {
-                                    options?.map(option =>
-                                        <Option key={ option.value } value={ option.value }>{ option.name }</Option>
-                                    )
-                                }
-                            </Select>
-                        </Stack>
-                    )
-                }
-            </Stack>
+            labels?.map(label =>
+                <Fieldset
+                    key={ label.value }
+                    title={ label.name }
+                >
+                    <Select
+                        name={ label.value }
+                        value={ value?.[label.value] }
+                        onValueChange={ v => onChange({ ...value, [label.value]: v }) }
+                    >
+                        {
+                            options?.map(option =>
+                                <SelectItem key={ option.value } value={ option.value }>{ option.name }</SelectItem>
+                            )
+                        }
+                    </Select>
+                </Fieldset>
+            )
         );
     }
 
     if (type === "select") {
         return (
-            <Select
-                placeholder={ placeholder || "Select" }
-                multiple={ multiple }
-                value={ handleSelectValue(value, multiple) }
-                onChange={ (_, value) => onChange(value) }
-            >
-                {
-                    options?.map(option =>
-                        <Option key={ option.value } value={ option.value }>{ option.name }</Option>
-                    )
-                }
-            </Select>
+            <Combobox
+                placeholder={ placeholder }
+                isMulti={ multiple }
+                value={ value }
+                onChange={ onChange }
+                options={ options }
+            />
         );
     }
 
     if (type === "textarea") {
         return (
             <Textarea
-                minRows={ 3 }
                 placeholder={ placeholder || "Type something..." }
                 value={ value || "" }
                 onChange={ e => onChange(e.target.value) }
