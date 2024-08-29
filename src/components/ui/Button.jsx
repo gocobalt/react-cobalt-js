30972193e6489974dd5bda1061de8858d2eaae09
TRACTION <19631364+iamtraction@users.noreply.github.com>
2024-08-29 16:45:03 +0530

    comp(Button): implement new props
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,21 +4,21 @@ import { gray } from "@radix-ui/colors";
 const Button = styled.button(props => ({
     all: "unset",
 
-    height: 35,
+    height: props.size === "small" ? 24 : 35,
     display: "inline-flex",
     alignItems: "center",
     justifyContent: "center",
-    padding: "0 16px",
+    padding: props.size === "small" ? "0 8px" : "0 16px",
     borderRadius: 4,
-    backgroundColor: props.color === "primary" ? gray.gray12 : gray.gray4,
-    fontSize: 15,
+    backgroundColor: props.color === "primary" ? gray.gray12 : props.color === "text" ? "transparent" : gray.gray4,
+    fontSize: props.size === "small" ? 12 : 15,
     fontWeight: 500,
     lineHeight: 1,
     color: props.color === "primary" ? gray.gray1 : gray.gray11,
     cursor: "pointer",
 
     "&:hover": {
-        backgroundColor: props.color === "primary" ? gray.gray11 : gray.gray5,
+        backgroundColor: props.color === "primary" ? gray.gray11 : props.color === "text" ? gray.gray3 : gray.gray5,
     },
 
     "&:focus": {
