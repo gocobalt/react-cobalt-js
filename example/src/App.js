f2edddb5689cb8f62cb1eed74b6e9e15181cafb5
TRACTION <19631364+iamtraction@users.noreply.github.com>
2025-05-15 14:47:17 +0530

    example: update
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "@cobaltio/react-cobalt-js";
 
 const App = () => {
-    const [ sessionToken, setSessionToken ] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiI2M2IzY2E1NzczYTIxMjQzMTZkMDRiNTgiLCJsaW5rZWRfYWNjb3VudF9pZCI6InRlc3QiLCJlbnZpcm9ubWVudCI6InRlc3QiLCJpYXQiOjE2OTY1OTMyOTMsImV4cCI6MTY5Njg1MjQ5M30.kQ0uU23xr_yqY_KwAt7Dd0gsVWn8cLJOvm3ZLjvgFOs");
+    const [ sessionToken, setSessionToken ] = useState("");
     const [ selectedApp, setSelectedApp ] = useState(null);
 
     // get Cobalt session token from Cobalt backend SDK
@@ -18,13 +18,16 @@ const App = () => {
 
     useEffect(() => {
         // get Cobalt session token and store it
-        // fetchSessionToken();
+        fetchSessionToken();
     }, []);
 
     return (
         // pass the cobalt session token to the Cobalt `Provider` and
         // wrap the Cobalt `Provider` component around the Cobalt `Config` component
-        <CobaltProvider baseApi="https://embedapi.gocobalt.io" sessionToken={ sessionToken }>
+        <CobaltProvider
+            baseApi="https://embedapi.gocobalt.io" // Optional. Only required if you're hosting Cobalt on premise.
+            sessionToken={ sessionToken }
+        >
             <div className="Page">
                 <div className="Header">Token: <code>{ sessionToken }</code></div>
                 <div className="Header">Applications</div>
@@ -49,16 +52,20 @@ const App = () => {
                    * by passing the app's `slug` (or `type`, if it's a native app) to the component.
                    */ }
                 <dialog open={ !!selectedApp }>
-                    <CobaltConfig
-                        id="CONFIG_ID" // Optional.
-                        slug={ selectedApp }
-                        // dynamic fields payload (optional, only required when you're using dynamic labels)
-                        fields={{ /* PAYLOAD */ }}
-                        style={{
-                            borderRadius: 8,
-                            maxWidth: 450,
-                        }}
-                    />
+                    {
+                        selectedApp && (
+                            <CobaltConfig
+                                id="CONFIG_ID" // Optional.
+                                slug={ selectedApp } // Optional.
+                                // dynamic fields payload (optional, only required when you're using dynamic labels)
+                                fields={{ /* PAYLOAD */ }}
+                                style={{
+                                    borderRadius: 8,
+                                    maxWidth: 450,
+                                }}
+                            />
+                        )
+                    }
                 </dialog>
             </div>
         </CobaltProvider>
